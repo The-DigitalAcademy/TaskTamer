d6cd54ad3b293c0a6119bb8a2a9bf3b9a5aeee9f
nene-hue <neomontshiwa2@gmail.com>
2026-01-19 16:30:40 +0200

    Sign up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { TaskForm } from './task-form/task-form';
+import { Signup } from './signup/signup';
 
 export const routes: Routes = [
     {path: 'home', component: Homepage},
-    {path: 'create-task', component: TaskForm}
+    {path: 'create-task', component: TaskForm},
+    {path: 'signup', component: Signup}
 ];
 
 
